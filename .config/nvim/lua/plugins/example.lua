4043ae00f9e862ed0e7f4be62357cb7438bb457a
Maghish <mail4mithran@gmail.com>
2024-11-04 10:22:25 +0530

    Modified .config/nvim
    Added wakatime.lua

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -9,7 +9,6 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
